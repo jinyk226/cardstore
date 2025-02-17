4f0069b9ffe40bee172fb13ade89afc1788bbeed
Jinyong Kim <jinyk226@gmail.com>
2025-02-17 12:34:10 -0800

    add sheet menu for mobile

---
+++ components/shared/header/Menu.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import ModeToggle from "./ModeToggle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Menu = () => {
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1">
+        <div className="flex space-x-2">
+          <ModeToggle />
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href="/sign-in">
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start">
+            <SheetTitle>Menu</SheetTitle>
+            <div>
+              <ModeToggle />
+            </div>
+            <Button asChild variant="ghost">
+              <Link href="/cart">
+                <ShoppingCart /> Cart
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href="/sign-in">
+                <UserIcon /> Sign In
+              </Link>
+            </Button>
+            <SheetDescription />
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
