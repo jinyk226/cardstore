e1dd2ba96e29c00772dde8ee0fe7093af93f6ec7
Jinyong Kim <jinyk226@gmail.com>
2025-02-17 11:01:57 -0800

    add header and footer

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import "@/assets/styles/globals.css";
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: `$s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+      <Footer />
+    </div>
+  );
+}
